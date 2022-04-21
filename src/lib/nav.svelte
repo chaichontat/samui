9b4f1ac400dbde550b5f4223564b4c8efc23869f
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-04-21 17:43:51 -0400

    Nav style
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -6,7 +6,7 @@
 <nav
   class="hidden gap-x-3 bg-gray-100 px-6 pb-3 shadow backdrop-blur dark:bg-gray-900 lg:flex lg:items-center"
 >
-  <div class="mt-2 text-xl font-medium">Navigation: Showing {$currRna.name}</div>
+  <div class="over mt-2 text-ellipsis text-xl font-medium">Showing <i>{$currRna.name}</i>.</div>
   <div class="flex-grow" />
   <Darkswitch />
   <div title="GitHub" class="">
