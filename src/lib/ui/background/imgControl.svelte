b63d978f7c70fad7afd06be4d4540d62786c186a
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-09-23 21:42:55 -0400

    Animation
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/lib/ui/background/imgControl.svelte
+++ src/lib/ui/background/imgControl.svelte
@@ -7,11 +7,14 @@
     type ImgCtrl
   } from '$src/lib/ui/background/imgColormap';
   import { zip } from 'lodash-es';
-  import { scale } from 'svelte/transition';
+  import { onMount } from 'svelte';
   import type { Background } from './imgBackground';
 
   export let background: Background;
   export let small = false;
+
+  let table: HTMLDivElement;
+  let cell: HTMLTableCellElement;
 
   const image = background.image;
   const channels = background.image?.channels;
@@ -53,74 +56,90 @@
     }
   }
 
+  onMount(() => {
+    if (table) {
+      const shrink = () => (table.style.maxWidth = `${cell.clientWidth + 10}px`);
+      table.addEventListener('mouseenter', () => (table.style.maxWidth = '2000px'));
+      table.addEventListener('mouseleave', shrink);
+      setTimeout(shrink, 1000);
+    }
+  });
+
   $: if (imgCtrl) background?.updateStyle(imgCtrl);
 </script>
 
 {#if imgCtrl}
-  {#if imgCtrl.type === 'composite' && Array.isArray(channels)}
-    <!-- w-[1%] is to set cell width to content and not stretch to fit the container. -->
-
-    <table class="table-auto">
-      <tbody class="hide-second-col">
-        {#each channels as name}
-          <tr class="">
-            <td class="w-[1%]" on:click={() => handleClick(name, imgCtrl.variables[name].color)}>
-              <button
-                class={classes(
-                  imgCtrl.variables[name].enabled
-                    ? bgColors[colors.findIndex((x) => x === imgCtrl.variables[name].color)]
-                    : 'opacity-60',
-                  imgCtrl.variables[name].enabled &&
-                    ['white', 'yellow'].includes(imgCtrl.variables[name].color)
-                    ? 'text-black'
-                    : '',
-                  `transition-width mx-auto my-0.5 flex items-center rounded-full px-3`
-                )}
+  <div
+    bind:this={table}
+    class="group flex max-w-[1000px] flex-col overflow-x-hidden rounded-lg bg-slate-200/80 bg-opacity-80 p-2 font-medium backdrop-blur-lg transition-all duration-1000 ease-in-out  dark:bg-slate-800/80"
+  >
+    {#if imgCtrl.type === 'composite' && Array.isArray(channels)}
+      <table class="table-auto">
+        <tbody class="hide-second-col">
+          {#each channels as name}
+            <tr class="">
+              <td
+                class=""
+                on:click={() => handleClick(name, imgCtrl.variables[name].color)}
+                bind:this={cell}
               >
-                {name}
-              </button>
-            </td>
-
-            <td class="flex items-center justify-center gap-x-1.5">
-              {#each zip(colors, bgColors) as [color, bg]}
                 <button
-                  on:click={() => handleClick(name, color)}
                   class={classes(
-                    bg,
-                    color !== 'white' ? 'opacity-90' : '',
-                    `transition-width mx-[1px] my-0.5 flex h-[18px] w-[18px] items-center rounded-full hover:opacity-100`
+                    imgCtrl.variables[name].enabled
+                      ? bgColors[colors.findIndex((x) => x === imgCtrl.variables[name].color)]
+                      : 'opacity-60 hover:opacity-80',
+                    imgCtrl.variables[name].enabled &&
+                      ['white', 'yellow'].includes(imgCtrl.variables[name].color)
+                      ? 'text-black'
+                      : '',
+                    `transition-width m-0.5 mx-auto flex items-center rounded-full pl-3 pr-4`
                   )}
+                >
+                  <div class="-translate-y-[1px]">{name}</div>
+                </button>
+              </td>
+
+              <td class="flex items-center justify-center gap-x-1.5">
+                {#each zip(colors, bgColors) as [color, bg]}
+                  <button
+                    on:click={() => handleClick(name, color)}
+                    class={classes(
+                      bg,
+                      color !== 'white' ? 'opacity-90' : '',
+                      `mx-[1px] my-1 flex h-[18px] w-[18px] items-center rounded-full opacity-70 transition-opacity duration-500 group-hover:opacity-100`
+                    )}
+                  />
+                {/each}
+              </td>
+              <td>
+                <input
+                  type="range"
+                  min="0"
+                  max="254"
+                  class="mx-4 min-w-[4rem] max-w-[8rem] cursor-pointer opacity-70 transition-opacity duration-500 group-hover:opacity-100"
+                  bind:value={imgCtrl.variables[name].max}
                 />
-              {/each}
-            </td>
-            <td>
-              <input
-                type="range"
-                min="0"
-                max="254"
-                class="ml-4 min-w-[4rem] max-w-[8rem] cursor-pointer"
-                bind:value={imgCtrl.variables[name].max}
-              />
-            </td>
-          </tr>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    {:else}
+      <div class="grid grid-cols-3 gap-y-1.5 gap-x-1">
+        {#each ['Exposure', 'Contrast', 'Saturation'] as name}
+          <div class="px-1">{small ? name.slice(0, 3) : name}:</div>
+          <input
+            type="range"
+            min="-0.5"
+            step="0.01"
+            max="0.5"
+            bind:value={imgCtrl[name]}
+            class="col-span-2 min-w-[4rem] max-w-[12rem] cursor-pointer"
+          />
         {/each}
-      </tbody>
-    </table>
-  {:else}
-    <div class="grid grid-cols-3 gap-y-1.5 gap-x-1">
-      {#each ['Exposure', 'Contrast', 'Saturation'] as name}
-        <div class="px-1">{small ? name.slice(0, 3) : name}:</div>
-        <input
-          type="range"
-          min="-0.5"
-          step="0.01"
-          max="0.5"
-          bind:value={imgCtrl[name]}
-          class="col-span-2 min-w-[4rem] max-w-[12rem] cursor-pointer"
-        />
-      {/each}
-    </div>
-  {/if}
+      </div>
+    {/if}
+  </div>
 {/if}
 
 <style lang="postcss">
@@ -130,9 +149,9 @@
     transition-duration: 1000ms;
   }
 
-  .hide-second-col td:nth-child(2),
+  /* .hide-second-col td:nth-child(2),
   .hide-second-col td:nth-child(3) {
-    @apply hidden;
+    @apply w-0;
   }
 
   .hide-second-col:hover td:nth-child(2) {
@@ -141,5 +160,5 @@
 
   .hide-second-col:hover td:nth-child(3) {
     @apply table-cell;
-  }
+  } */
 </style>
