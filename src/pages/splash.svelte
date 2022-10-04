b31590fda1fc927e5af4fc691b97e72032108155
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-10-04 18:38:57 -0400

    Feedback
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/pages/splash.svelte
+++ src/pages/splash.svelte
@@ -2,17 +2,20 @@
   import preload from '$lib/data/preload';
   import Github from '$src/lib/components/github.svelte';
   import { byod } from '$src/lib/data/byod';
+  import Feedback from '$src/lib/sidebar/Feedback.svelte';
   import { Plus } from '@steeze-ui/heroicons';
   import { Icon } from '@steeze-ui/svelte-icon';
 </script>
 
 <!-- Splash import -->
-<nav class="flex w-full justify-between py-[18px] pl-4 pr-4">
+<nav class="flex w-full justify-between py-[18px] pl-4 pr-4 gap-x-4">
   <div class="text-2xl font-medium text-yellow-300">Loopy Browser</div>
+  <div class="flex-grow" />
+  <Feedback />
   <Github />
 </nav>
 
-<div class="center flex h-full w-full flex-col items-center justify-center gap-y-5">
+<div class="center flex flex-col items-center justify-center gap-y-5">
   <div class="flex flex-col items-center">
     <button
       class="splash-button group mb-1 rounded-lg bg-gradient-to-br from-purple-600 to-pink-500 text-xl font-medium text-neutral-50 ring-pink-800"
@@ -42,8 +45,8 @@
   >
     <span class="px-5 py-2 group-hover:bg-opacity-0">Show Visium-IF Data</span>
   </button>
+</div>
 
-  <div class="absolute bottom-5 left-1/2 translate-x-[-50%] text-center text-neutral-300">
-    All processing is done locally. Your data never leave your computer.
-  </div>
+<div class="absolute bottom-5 left-1/2 translate-x-[-50%] text-center text-neutral-300">
+  All processing is done locally. Your data never leave your computer.
 </div>
