3f2b41aa973b24d7dcff26581200b611d12ad0b0
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-10-27 12:31:16 -0400

    Exception error
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import '../app.css';
+
+  onMount(() => {
+    window.onunhandledrejection = (e: PromiseRejectionEvent) => {
+      alert('Unhandled error: ' + (e.reason as string));
+    };
+  });
 </script>
 
 <slot />
