c2bd0104d5f885a227ff7836d6a20471c8c35d4a
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2023-01-27 13:22:04 -0500

    handleerror
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,8 @@ export function classes(...classes: (false | null | undefined | string)[]): stri
 export function rand() {
   return Math.random().toString(36).substring(2);
 }
+
+export function handleError(e: Error) {
+  console.error(e);
+  alert(`${e?.message ?? e?.reason ?? e} ${e?.stack}`);
+}
