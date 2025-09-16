62fc5e95273c6989edb32c9fa23a566a9e5fd7e5
Chaichontat Sriworarat <csriwor1@jh.edu>
2025-09-16 18:06:11 -0400

    cn
    Signed-off-by: Chaichontat Sriworarat <csriwor1@jh.edu>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,9 @@
-export type Named<T> = { name: string; values: T };
+import type { ClassValue } from 'clsx';
 
-export function classes(...classes: (false | null | undefined | string)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export type Named<T> = { name: string; values: T };
 
 export function rand() {
   return Math.random().toString(36).substring(2);
@@ -12,3 +13,9 @@ export function handleError(e: Error, stack = false) {
   console.error(e);
   alert(`${e?.message ?? e?.reason ?? e} ${stack ? e?.stack : ''}`);
 }
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const classes = cn;
